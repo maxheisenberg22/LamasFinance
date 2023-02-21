326fc0e7ff561548b7258a81c5a0fbf9241e49b9
remtori <lqvu99@gmail.com>
2023-02-21 20:22:05 +0700

    up-or-down: test anchor intergration

---
+++ programs/up-or-down/src/lib.rs
@@ -0,0 +1,63 @@
+use anchor_lang::prelude::*;
+use chainlink_solana as chainlink;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod up_or_down {
+    use super::*;
+    pub fn execute(ctx: Context<Execute>) -> Result<()> {
+        let round = chainlink::latest_round_data(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info(),
+        )?;
+
+        let description = chainlink::description(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info(),
+        )?;
+
+        let decimals = chainlink::decimals(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info(),
+        )?;
+        // write the latest price to the program output
+        let decimal_print = Decimal::new(round.answer, u32::from(decimals));
+        msg!("{} price is {}", description, decimal_print);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Execute<'info> {
+    pub chainlink_feed: AccountInfo<'info>,
+    pub chainlink_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct Decimal {
+    pub value: i128,
+    pub decimals: u32,
+}
+
+impl Decimal {
+    pub fn new(value: i128, decimals: u32) -> Self {
+        Decimal { value, decimals }
+    }
+}
+
+impl std::fmt::Display for Decimal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut scaled_val = self.value.to_string();
+        if scaled_val.len() <= self.decimals as usize {
+            scaled_val.insert_str(
+                0,
+                &vec!["0"; self.decimals as usize - scaled_val.len()].join(""),
+            );
+            scaled_val.insert_str(0, "0.");
+        } else {
+            scaled_val.insert(scaled_val.len() - self.decimals as usize, '.');
+        }
+        f.write_str(&scaled_val)
+    }
+}
