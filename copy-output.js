3d6dd56c74997e8904dc4a105fc4227f3765c210
remtori <lqvu99@gmail.com>
2023-03-05 10:54:27 +0700

    build script: update copy targets

--- copy-output.js
+++ copy-output.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 
-fs.copyFileSync('./target/types/lucky_spinner.ts', './lucky_spinner.ts');
+fs.copyFileSync('./target/types/lucky_spinner.ts', './idl/lucky_spinner.ts');
+fs.copyFileSync('./target/types/jackport_lottery.ts', './idl/jackport_lottery.ts');
+fs.copyFileSync('./target/types/up_or_down.ts', './idl/up_or_down.ts');
+fs.copyFileSync('./target/types/price_predict.ts', './idl/price_predict.ts');
